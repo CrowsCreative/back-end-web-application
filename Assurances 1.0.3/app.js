cf0ecebd60df5c4afdf1cd087b2787df5e290456
The Crows <76985315+CrowsCreative@users.noreply.github.com>
2022-05-21 21:10:10 -1200

    upload web scrapping 1.0.3 version
    this is not fully functional app

---
+++ Assurances 1.0.3/app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app     = express();
+const port    = 10000;
+const index       = require("./routes/index");
+const scrapper    = require("./routes/scrapper");
+const fetcher    = require("./routes/fetcher");
+const bodyParser    = require("body-parser");
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(bodyParser.json());
+app.use(express.static(__dirname + "/public"));
+app.set("view engine", "ejs");
+
+app.use("/", index);
+app.use("/scrapper", scrapper);
+app.use("/fetcher", fetcher);
+
+app.listen(port, ()=>
+{
+    console.log(`server start on port ${port}`);
+})
